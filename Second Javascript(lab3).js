4ba3479d03dd1120b6fb8aa9a62f4bb7920cead6
Your Name <your@email.com>
2022-09-24 10:37:44 +0530

    Scripting

---
+++ Second Javascript(lab3).js
@@ -0,0 +1,19 @@
+/*<html>
+    <script>
+        var a=20;
+        if(a%2==0){
+            document.write("a is an even number");
+        }
+        else{
+            document.write("a is an odd number")
+        }
+    </script>
+</html>*/
+<html>
+    <script>
+        for (i=1; i<=5; i++)
+        {
+            document.write(i+"br/");
+        }
+    </script>
+</html>
